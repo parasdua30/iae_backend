f2ba271a9ae015e14bce168b739661d0d67b7645
Paras Dua <parasdua1130@gmail.com>
2024-10-12 18:21:51 +0530

    r

--- constants/config.js
+++ constants/config.js
@@ -3,6 +3,7 @@ const corsOptions = {
         "http://localhost:5173",
         "http://localhost:4173",
         process.env.CLIENT_URL,
+        "https://interview-at-ease.vercel.app/",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
