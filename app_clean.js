41097bef0c3cf91804cd882e7dc3cb7c3c645dbd
Paras Dua <parasdua1130@gmail.com>
2024-10-12 16:08:27 +0530

    r

--- app_clean.js
+++ app_clean.js
@@ -15,10 +15,6 @@ app.use(cors(corsOptions));
 const io = new Server(server, { cors: corsOptions });
 const lock = new AsyncLock();
 
-app.get("/", (req, res) => {
-    res.send("Hello World");
-});
-
 app.set("io", io);
 
 let editorContent = "";
@@ -47,28 +43,15 @@ io.on("connection", (socket) => {
     socket.on("room:join", (data) => {
         const { roomId, myName } = data;
         const username = myName;
-        // console.log(data);
+
         userToSocketIdMap.set(username, socket.id);
         socketidToUserMap.set(socket.id, username);
 
-        // io.to(room).emit("user:joined", { username, id: socket.id });
-        // socket.join(room);
-        // io.to(socket.id).emit("room:join", data);
-
         const clients = getAllConnectedClients(roomId);
 
-        // console.log("number of users in this room", clients);
-
         if (clients.length > 0) {
-            // console.log(
-            //     `total users in the room currently: ,
-            //     ${socket.id},
-            //     ${clients.length},
-            //     ${clients}`
-            // );
-            io.to(roomId).emit("user:joined", { username, id: socket.id }); // baaki room members ko send karna ki ek new user join ho gaya hai
+            io.to(roomId).emit("user:joined", { username, id: socket.id });
             io.to(socket.id).emit("room:join", {
-                // khud ko batana, ki jo other user hai uske baar mein
                 data: data,
                 msg: "id of other user of the room",
                 id: clients[0]?.socketId,
@@ -77,16 +60,7 @@ io.on("connection", (socket) => {
                 currentLanguage: currentLanguage,
             });
         }
-        console.log(socketidToUserMap);
         socket.join(roomId);
-    });
-
-    socket.on("editor:request", (name) => {
-        console.log("request from", name);
-        socket.emit("editor:update", {
-            content: editorContent,
-            language: currentLanguage,
-        });
     });
 
     socket.on("leave:room", ({ roomId, username }) => {
@@ -103,12 +77,10 @@ io.on("connection", (socket) => {
     });
 
     socket.on("peer:nego:needed", ({ to, offer }) => {
-        // console.log("peer:nego:needed", offer);
         io.to(to).emit("peer:nego:needed", { from: socket.id, offer });
     });
 
     socket.on("peer:nego:done", ({ to, ans }) => {
-        // console.log("peer:nego:done", ans);
         io.to(to).emit("peer:nego:final", { from: socket.id, ans });
     });
 
@@ -151,13 +123,10 @@ io.on("connection", (socket) => {
 
     // CodeEditor
     socket.on("using:editor", ({ fellowUsername, roomId }) => {
-        // socket.broadcast.emit("fellow:using:editor", { fellowUsername });
-        // console.log(`telling that ${fellowUsername} using:editor`);
         socket.to(roomId).emit("fellow:using:editor", { fellowUsername });
     });
 
     socket.on("codeChange", ({ code, roomId }) => {
-        // Acquire a lock before changing the code state
         lock.acquire("codeLock", (done) => {
             // codeState = newCode;
             // Broadcast the new code to all clients except the sender
@@ -177,43 +146,6 @@ io.on("connection", (socket) => {
         currentLanguage = language;
         io.to(roomId).emit("langChange", language);
     });
-
-    /* discarded
-        socket.on("room:join", (data) => {
-            const { roomId, username } = data;
-            userToSocketIdMap.set(username, socket.id);
-            socketIdToUserMap.set(socket.id, username);
-
-            socket.to(roomId).emit("user:joined", { username, id: socket.id }); // broadcasting to all the memebers of this room.
-            socket.join(roomId); // let the new user join this room.
-            io.to(socket.id).emit("room:join", {
-                ...data,
-                mySocketId: socket.id,
-            }); // let the new user know that he has joined the room.
-        });
-
-        socket.on("user:call", ({ to, offer }) => {
-            io.to(to).emit("incoming:call", { from: socket.id, offer });
-        });
-
-        socket.on("call:accepted", ({ to, ans }) => {
-            io.to(to).emit("call:accepted", { from: socket.id, ans });
-        });
-
-        // socket.on("disconnect", () => {
-        //     console.log("Disconnected");
-        // });
-
-        socket.on("peer:nego:needed", ({ to, offer }) => {
-            console.log("peer:nego:needed", offer);
-            io.to(to).emit("peer:nego:needed", { from: socket.id, offer });
-        });
-
-        socket.on("peer:nego:done", ({ to, ans }) => {
-            console.log("peer:nego:done", ans);
-            io.to(to).emit("peer:nego:final", { from: socket.id, ans });
-        });
-    */
 });
 
 function error(err, req, res, next) {
